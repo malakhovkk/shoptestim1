43b2e84458933541de18ffa97c4dd054059e0350
Your Name <you@example.com>
2021-06-05 00:28:07 +0300

    fix2

--- site_new/settings.py
+++ site_new/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Application definition
 MIDDLEWARE_CLASSES = (
     # Simplified static file serving.
